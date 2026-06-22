0dec34d895fa4779dfbf36f91e59b0d279942e25
Ritesh <riteshparmar17@gmail.com>
2026-06-22 18:12:52 -0400

    added custom not found page

---
+++ src/app/core/pages/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+  constructor(
+    private router: Router
+  ) { }
+
+  goToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
